45682c17e0232aa3c9874121bac085445e459491
Casper Andersson <andersson.casper99@gmail.com>
2020-05-05 11:41:39 +0200

    Added guards for base_packet.h

--- src/shared/base_packet.h
+++ src/shared/base_packet.h
@@ -1,6 +1,15 @@
+#ifndef BASE_PACKET_H
+#define BASE_PACKET_H
+
+
 typedef struct{
   int seq; // 4 bytes
   int ack; // 4 bytes 
   unsigned char flags; // ACK, NACK, SYN, FIN // 1 byte
   char data[64]; // ? bytes
 } base_packet;
+
+
+
+
+#endif
