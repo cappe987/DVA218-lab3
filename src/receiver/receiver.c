deaf725b9997bf97fd3def2ed7b5a77d77115d1f
Nick <spec_89@hotmail.com>
2020-05-05 09:14:10 +0200

    Inserted code and added the basic message struct

--- src/receiver/receiver.c
+++ src/receiver/receiver.c
@@ -1,7 +1,64 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+  
+#define PORT     8080 
+#define MAXLINE 1024 
 
-
-int main(){
-  printf("Hello, World!\n");
-  return 0;
-}
+struct windowPacket{
+  int seq; // 4 bytes
+  int ack; // 4 bytes 
+  unsigned char flags; // ACK, NACK, SYN, FIN // 1 byte
+  char data[64]; // ? bytes
+};
+  
+// Driver code 
+int main() { 
+    int sockfd; 
+    char buffer[MAXLINE]; 
+    char *hello = "Hello from server"; 
+    struct sockaddr_in servaddr, cliaddr; 
+      
+    // Creating socket file descriptor 
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+      
+    memset(&servaddr, 0, sizeof(servaddr)); 
+    memset(&cliaddr, 0, sizeof(cliaddr)); 
+      
+    // Filling server information 
+    servaddr.sin_family    = AF_INET; // IPv4 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+    servaddr.sin_port = htons(PORT); 
+      
+    // Bind the socket with the server address 
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
+            sizeof(servaddr)) < 0 ) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+      
+    int len, n; 
+  
+    len = sizeof(cliaddr);  //len is value/resuslt 
+  
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
+                MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
+                &len); 
+    buffer[n] = '\0'; 
+    printf("Client : %s\n", buffer); 
+    sendto(sockfd, (const char *)hello, strlen(hello),  
+        MSG_CONFIRM, (const struct sockaddr *) &cliaddr, 
+            len); 
+    printf("Hello message sent.\n");  
+      
+    return 0; 
+} 
