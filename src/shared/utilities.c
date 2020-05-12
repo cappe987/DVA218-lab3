7dd650b3c2217d5ebddd3e3387c5f2e5d1ad0310
Casper Andersson <andersson.casper99@gmail.com>
2020-05-12 09:58:08 +0200

    Fixed bug with send_without_data [See details]
    send_without_data wasn't sending it properly. socklen had to be set to the size of the address. 
    Also fixed debug messages.

--- src/shared/utilities.c
+++ src/shared/utilities.c
@@ -26,21 +26,21 @@ void increment_timeout(int *timeout, int *response, int sockfd, struct timeval *
 }
 
 ssize_t send_without_data(int seq, int flag, int sockfd, struct sockaddr_in sockaddr){
-  socklen_t len = 0;
+  socklen_t len = sizeof(sockaddr);
   base_packet packet;
   packet.data[0] = '\0';
   packet.seq = seq;
   packet.flags = flag;
   printf("Sent ");
-  if(flag == 1){printf("ACK"); flag = flag - 1;}
-  if(flag == 2){printf("SYN"); flag = flag - 2;}
-  if(flag == 4){printf("FIN"); flag = flag - 4;}
-  if(flag == 8){printf("NACK");}
+  if(flag % 2  == 1){printf("ACK "); flag = flag - 1;}
+  if(flag % 4  == 2){printf("SYN "); flag = flag - 2;}
+  if(flag % 8  == 4){printf("FIN "); flag = flag - 4;}
+  if(flag % 16 == 8){printf("NACK ");}
 
   printf("\n");
 
   crc_packet full_packet = create_crc((char*)&packet);
   return send_with_error(sockfd, (const char *)&full_packet, sizeof(crc_packet),  
-      MSG_CONFIRM, (const struct sockaddr *) &sockaddr, len); 
+      MSG_CONFIRM, (const struct sockaddr *)&sockaddr, len); 
 
 }
